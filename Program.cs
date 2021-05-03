d5d659f671ccab4acc4be36553f706a5805a39b8
James Ferguson <jimmiexd@gmail.com>
2021-05-03 01:15:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+using ImageMagick;
+
+using ShellProgressBar;
+
+string dir = $"{new FileInfo(args[0]).DirectoryName}/Old";
+
+if (!Directory.Exists(dir)) {
+    _ = Directory.CreateDirectory(dir);
+}
+
+int totalTicks = args.Length;
+
+ProgressBarOptions progressBarOptions = new ProgressBarOptions {
+    ProgressCharacter = '─',
+    ProgressBarOnBottom = true
+};
+
+ProgressBar progressBar = new(totalTicks, "Converting Images", progressBarOptions);
+
+foreach (string file in args) {
+
+    progressBar.Message = $"Converting {Path.GetFileName(file)}";
+    //Console.WriteLine($"Converting {Path.GetFileName(file)}");
+    MagickImage image = new(file);
+    image.Write($"{Path.GetFileNameWithoutExtension(file)}.webp");
+    //Console.WriteLine($"Done!");
+    progressBar.Tick();
+
+    File.Move(file, $"{dir}/{Path.GetFileName(file)}");
+}
+
+//Console.WriteLine("Press any button to close");
+_ = Console.ReadKey();
